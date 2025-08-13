1ec07d6e06f8e0b514f3da2d78b3e3fae6f29e2b
Karl Skagersten <karl.skagersten@gmail.com>
2025-08-13 19:23:46 +0200

    feat: add summary 2025 and new meetings page

---
+++ pages/referat/2025.js
@@ -0,0 +1,125 @@
+import styles from "../../styles/Home.module.css";
+import Head from "next/head";
+import Link from "next/link";
+
+const YearlyStory = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Matteröds Hembygdsförening</title>
+        <meta name="description" content="Matteröds Hembygdsförenings hemsida"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      <main className={styles.main}>
+        <div style={{ paddingBottom: '0.5rem', borderBottom: '1px solid #eaeaea' }}>
+          <h1 className={styles.title}>
+            Matteröds Hembygdsförening
+          </h1>
+        </div>
+        
+        <p className={styles.link}>
+          <Link href="/">
+            {"< Till startsidan"}
+          </Link>
+        </p>
+
+        <h2>
+          Referat från Hembygdsdagen i Matteröd söndagen den 3 augusti 2025
+        </h2>
+        <p className={styles.description}>
+          Dagen inleddes med ett besök på Tyringe Skolmuseum där Sven-Ingvar Jönsson samlade oss i ett klassrum på
+          Tyringe skola. Där berättade han mycket medryckande om vad som finns i den gamla lärar- och
+          vaktmästarbostaden, troligen byggt omkring 1915 som idag är Tyringe Skolmuseum. Flera hade tagit med sig
+          fotografier från skoltiden så att skärmutställningen om Matteröds skolor ytterligare kunde utökas.
+          <br/>
+          Bland annat berättade Sven-Ingvar om Anna Mobergs träbollar i småskolans räknelåda från 1907. En sådan låda
+          finns på
+          Göteborgs skolmuseum, i Tyringe och kanske på någon annan plats i Sverige. Vidare berättade han om den
+          kombinerade Benson-skolorgeln och katedern som två orgelbyggare hjälpt till att laga och kantor Ingvar Ahlbin
+          har hållit högtidlig invigning på. Finsnickaren och torparen Ola Nilsson som var gift med Maria Kask (torp
+          nr.1 Isakstorp i boken Ödetorp och torparöden) arbetade på Bensons orgelfabrik i Hässleholm fr.o.m 1887. Tänk
+          om det var han som byggde den från början!
+        </p>
+        <p className={styles.description}>
+          Förr fanns det 19 skolor i det som var Tyringe gamla kommun. Idag finns endast 4 kvar, Västra Torup, Röke,
+          Finja och Tyringe. Matteröd var föregångare med skola redan 1830. När Matteröd hade sin befolkningstopp
+          1877-78 med 1175 invånare fanns det 189 barn inskrivna i 3 skolor i Matteröd. Småskolan som fram till 1885
+          fick inhysas i en lokal hos lantbrukare Per Nilsson(Björn) på Matteröd nr.5. Skolan som togs i bruk 1854 och
+          idag är församlingshem och skolan i Västra Bränner byggd 1874. Totalt var 3 lärare anställda för alla dessa
+          barn. Matteröds utgifter för skolan detta år var 717 kronor varav 650 kr kom tillbaks i form av statsbidrag.
+          91 kr anslogs till inventarier och tre kr till läromedel!
+          <br/>
+          Alla de cirka 50 närvarande fick dessutom skriva i gästboken med de gamla bläckpennorna. Det lyckades
+          förträffligt utan plumpar i protokollet.
+        </p>
+        <p className={styles.description}>
+          Därefter intogs köttbullar, sås och potatis med tillbehör på Mikas bar och restaurang vid
+          Förmedlingscentralen, Tyringe. Efter denna välsmakande måltid tog vi oss till Matteröds församlingshem för
+          årsmöte. Styrelsen omvaldes i sin helhet och det hade blivit ett gott resultat i kassaomslutningen, mycket
+          tack vare att bankräntan under räkenskapsåret varit högre än på flera år. Uno Persson avgick som revisor och i
+          hans ställe nyvaldes Gunilla Vilnersson, Mjölkalånga.
+        </p>
+        <p className={styles.description}>
+          Vid den efterföljande årshögtiden inledde Daniel Johansson med att läsa en dikt av Nelly Sten, Måleböke.
+          Dikten beskrev dramatiskt hur en pojke trillade ner i brunnen vid Matteröds skola i slutet av 1800-talet. Han
+          skulle hämta in vatten till skolbarnen men förolyckades. Dikten beskrev sedan hur han räddades av skolläraren
+          och allt slutade lyckligt. Pojken blev inte ens förkyld och i vuxen ålder blev han byggmästare i Malmö. Vilket
+          levnadsöde!
+        </p>
+        <p className={styles.description}>
+          Sedan spelade gruppen &quot;Släktbandet&quot; flera musikstycken vid ett flertal tillfällen varvat med
+          föredrag av Leif
+          Önnerby. Han berättade om sin första lärartjänst då han som 22-åring kom till Matteröd 1956-57 kombinerat med
+          kantorstjänst. Han hade på våren 1956 utexaminerats från lärarhögskolan i Kristianstad och tagit kantorsexamen
+          på sommaren. Jobbet innebar arbete med skolan sex dagar i veckan och på söndagar kantorstjänst. Han bodde på
+          andra våningen i det som idag är församlingshem, så han hade nära till skolan. Det hanns under skoldagarna
+          inte med att laga middag under den korta middagsrasten, så det var svårt med måltiderna tills en snäll dam i
+          församlingen ställde upp och lagade middagsmåltid till honom. Det fanns ingen kyl, frys eller elspis. Själv
+          elda i vedspis och utedass på gården var kallt på vintern. Dessutom fanns inga gatlyktor så det var väldigt
+          mörkt om kvällarna. Han berättade att han &quot;som tur var inte var mörkrädd eller var rädd för spöken, men
+          han
+          hade ändå respekt för dem&quot;. När han inte hann elda fick han svepa in sig i en extra filt och mössa på
+          huvudet
+          när han skulle sova. Leif underhöll även med flera pianostycken.
+        </p>
+        <p className={styles.description}>
+          Erika Svensson delgav sedan sina minnen från Tostarp och Matteröds skola från 30 oktober 1955 till mars 1956.
+          Då flyttade familjen vidare till Tollered som ligger mellan Göteborg och Alingsås. Erikas mamma hade flytt
+          från Tallinn, Estland 1944 till Österrike. 1955 hade familjen fått kontakt med Gösta Thulin, Lutherska
+          Världsförbundet. Han ordnade så att familjen kom till Tostarp 1955 och mottogs av pastorsparet Erik och Sigrid
+          Eriksson som den första kvällen bjöd på köttsoppa och knäckebröd. Denna sorts bröd hade Erika aldrig smakat
+          förut, men hon tyckte allt var gott och det var ett väldigt omtänksamt och vänligt mottagande. Hon mindes även
+          när det kom mycket snö och det kastades snöboll och byggdes snölyktor. Det blev intervju och bilder i
+          tidningar. Bl.a en artikel av Einar Rimmerfors i tidningen Svensk Veckotidning &quot;Jul på Tostarpsgården
+          1955&quot;.
+          <br/>
+          Erikas dotter Johanna berättade om att hon håller på med efterforskningar om sin mors och mormors
+          livsberättelser om hur det såg ut i Österrike och Estland 1944. Efter Johanna gått någon skrivarkurs bl.a på
+          folkhögskola har mormors berättelse kommit med i en bok, en antologi som heter &quot;Vändpunkter&quot;.
+        </p>
+        <p className={styles.description}>
+          Så knöts berättelser samman 2025 om en av Hembygdsföreningens grundare Axel Andersson och om hans arbete på
+          Tostarp som ett antal år var flyktingförläggning och att det har varit betydelsefullt för flera människor.
+          Alla hade sedan mycket att samtala omkring det välsmakande kaffebordet, där förhoppningar knöts att mötas igen
+          om ett år den första söndagen i augusti.
+          <br/>
+          Alla deltagare uppmanades att inkomma med förslag till programpunkter inför kommande år.
+        </p>
+        <p className={styles.description}>
+          Referent: Bo Nilsson
+        </p>
+        <p className={styles.link}>
+          <Link href="/">
+            {"< Till startsidan"}
+          </Link>
+        </p>
+      </main>
+      <footer className={styles.footer}>
+        Styrelsen genom: Daniel Johansson, tel. 0704-38 30 48, Majvi Larsson, tel. 076-584 04 43
+      </footer>
+    </div>
+  )
+}
+
+export default YearlyStory
